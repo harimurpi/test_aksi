dca80d6ea64dda509bf928046d0eaf524a41d22b
Alfian76 <malfian76@gmail.com>
2017-04-10 13:40:49 +0700

    - content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AboutComponent }  from './components/pages/about.component';
 import { HomeComponent }  from './components/pages/home.component';
 import { ContactComponent }  from './components/pages/contact.component';
 import { MoviesComponent }  from './components/pages/movies.component';
+import { FooterComponent }  from './components/footer/footer.component';
+import { SidebarComponent }  from './components/sidebar/sidebar.component';
 import {routing} from './app.routing';
 
 @NgModule({
   imports:      [ BrowserModule, routing],
-  declarations: [ AppComponent, NavbarComponent, JumbotronComponent, AboutComponent, HomeComponent, ContactComponent, MoviesComponent ],
+  declarations: [ AppComponent, NavbarComponent, JumbotronComponent, AboutComponent, HomeComponent, ContactComponent, MoviesComponent, FooterComponent, SidebarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/components/jumbotron/jumbotron.component.ts
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: `jumbotron.component.html`
 })
 export class JumbotronComponent  {
-	private jbtHeading:string;
-	private jbtText:string;
-	private jbtBtnText:string;
-	private jbtBtnUrl:string;
-
-	constructor(){
-		this.jbtHeading ="Hello World";
-		this.jbtText ="bla bla bla bla bla bla bla....";
-		this.jbtBtnText ="Read More";
-		this.jbtBtnUrl ="/about";
-	}
+	
 }
